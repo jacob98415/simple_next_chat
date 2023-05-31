e1d7efb592571d4d9323664036bea7588a4b4aa6
Grussi48 <carterrobinson48@gmail.com>
2023-05-31 18:11:56 +0900

    linearblue#added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,10 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  <main className={`h-screen bg-gradient-to-r from-sky-500 to-indigo-500`}>
+  return(
+    <main className={`h-screen bg-gradient-to-r from-sky-500 to-indigo-500`}>
 
   </main>
+  );
+  
 }
