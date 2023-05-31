13bc13380ef03a85d13d90abfb20e500934c545a
luckybutter <133196673+luckybutter322982@users.noreply.github.com>
2023-05-31 16:50:35 +1000

    navbar index
    Signed-off-by: luckybutter <133196673+luckybutter322982@users.noreply.github.com>

---
+++ src/components/navbar.tsx
@@ -0,0 +1,130 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button */}
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {/* Icon when menu is closed. */}
+              {/* Menu open: "hidden", Menu closed: "block" */}
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              {/* Icon when menu is open. */}
+              {/* Menu open: "block", Menu closed: "hidden" */}
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Logo */}
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <Link href="/" legacyBehavior>
+                <a className="text-white text-xl font-bold">Your Logo Here</a>
+              </Link>
+            </div>
+          </div>
+
+          {/* Links */}
+          <div className="hidden sm:block sm:ml-6">
+            <div className="flex space-x-4">
+              <Link href="/" legacyBehavior>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Home
+                </a>
+              </Link>
+              <Link href="/about" legacyBehavior>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  About
+                </a>
+              </Link>
+              <Link href="/admin" legacyBehavior>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Admin
+                </a>
+              </Link>
+              <Link href="/login" legacyBehavior>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  login
+                </a>
+              </Link>
+              <Link href="/products" legacyBehavior>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  products
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu, toggle classes based on menu state. */}
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <Link href="/" legacyBehavior>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Home
+            </a>
+          </Link>
+          <Link href="/admin" legacyBehavior>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Admin
+            </a>
+          </Link>
+          <Link href="/about" legacyBehavior>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              About
+            </a>
+          </Link>
+          <Link href="/login" legacyBehavior>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Login
+            </a>
+          </Link>
+          <Link href="/products" legacyBehavior>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Products
+            </a>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
