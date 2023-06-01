5332c915ab419ecdf11d85757412125034cb695f
lionstar <132696312+lionstar259007@users.noreply.github.com>
2023-06-01 00:34:48 -0700

    jorigin/master head commit

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -55,7 +55,7 @@ export default function Page() {
     //     </button>
     //   </div>
     // </div>
-
+    //
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 min-h-screen ">
       <div className="flex flex-1 justify-between sm:hidden">
         <a
